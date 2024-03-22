44473efa075d351be3175589fc307dc1ba24ae80
Jhan Frank <jhnfrankz@gmail.com>
2024-03-21 20:21:34 -0500

    Stage 1/7:Game log

--- src/main/java/com/jhnfrankz/bullsandcows/Main.java
+++ src/main/java/com/jhnfrankz/bullsandcows/Main.java
@@ -2,6 +2,16 @@ package com.jhnfrankz.bullsandcows;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("""
+                The secret code is prepared: ****.
+                                
+                Turn 1. Answer:
+                1234
+                Grade: None.
+                                
+                Turn 2. Answer:
+                9876
+                Grade: 4 bulls.
+                Congrats! The secret code is 9876.""");
     }
 }
